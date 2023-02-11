981ef844a63cd0bc2c4a14365859eb50eafe0084
Joanne Lewis <lewisj20@mcmaster.ca>
2023-02-11 15:01:28 -0500

    Merged sites, have not refactored yet

---
+++ view/index.php
@@ -0,0 +1,12 @@
+<main>
+    <div>
+        <h1> Match with people  that can explain technology down to the basics, in a simple format</h1>
+        <div class="img-div">
+            <img src="/images/cheering.png" alt="Cheering People"/>
+        </div>
+    </div>
+    <div class="support-div">
+        <h1>Get Support Now</h1>
+        <button class="submit-button"> Get Started </button>
+    </div>
+</main>
