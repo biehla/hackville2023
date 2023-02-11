889ca51d0e0dcd8b6b6bada0eadcc81535c4e3ba
Anais <anaiszed447@gmail.com>
2023-02-11 14:04:53 -0500

    Implement navigation to button click

---
+++ script.js
@@ -0,0 +1,7 @@
+//access the button
+let button = document.querySelector(".submit-button")
+
+//submit button click event
+button.addEventListener('click', ()=>{
+    window.location.href = "permission_page.html";
+})
