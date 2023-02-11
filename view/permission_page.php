61a82197d72e09ce8c834150a324ef9abf4e74ed
Joanne Lewis <lewisj20@mcmaster.ca>
2023-02-11 16:52:00 -0500

    i have no clue what I changed have fun 🎲🎲

---
+++ view/permission_page.php
@@ -0,0 +1,22 @@
+<header>
+    <div>
+        <p> Help.Tech</p>
+    </div>
+</header>
+
+<main>
+
+
+<div>
+    <h3> Allow location information to be used to find matches?</h3>
+    <h3> This will only last for the search session. Learn more here.</h3>
+</div>
+
+<div>
+    <img src="../images/Finance,%20e-commerce,%20startup,%20workflow%20_%20account,%20information,%20user,%20profile,%20data.png"/>
+</div>
+
+<div class="buttons-div">
+    <button class="yes-button"> YES</button>
+    <button class="no-button"> NO</button>
+</div>
